91bbe8d6461dce02bec2c2c39bb05131b83d896d
mk700 <mk8557823725@gmail.com>
2020-10-19 20:29:46 +0530

    Project initialization

---
+++ src/main/java/jarvis/mk/smkpetclinic/SmkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package jarvis.mk.smkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmkPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jarvis/mk/smkpetclinic/SmkPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package jarvis.mk.smkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmkPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
